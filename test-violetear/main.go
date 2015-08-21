1b4e3b8fbe5fd6b32aeb6e6aca7b8a9102d9030b
nbari <nbari@dalmp.com>
2015-08-21 16:58:24 +0200

    modified:   main.go

--- test-violetear/main.go
+++ test-violetear/main.go
@@ -1,21 +1,24 @@
 package main
 
 import (
-	_ "fmt"
+	"fmt"
 	"github.com/nbari/violetear"
+	//	"log"
+	"net/http"
 )
 
+func hello_world(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
 func main() {
-	router := violetear.New()
+	router := violetear.New(true)
 
 	router.AddRegex(":uuid", `[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}`)
-	router.AddRegex(":ip", `^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$`)
 
-	router.Add("/hello/world", "hello_world")
-	router.Add("/hello/world/get", "hello_world", "GET")
-	router.Add("/", "main")
-	router.Add(":uuid", "h_uuid")
-	router.Add("/ip/:ip", "h_uuid")
+	router.AddPath("/hello/world", hello_world)
+	router.AddPath(":uuid", hello_world)
 
-	//	log.Fatal("teste", router)
+	router.Run(":8080")
+
 }
