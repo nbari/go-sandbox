8a5372b2a36cea34fc96c13cfb4ad767d581f2ef
nbari <nbari@dalmp.com>
2015-08-26 16:42:33 +0200

    modified:   main.go

--- test-violetear/main.go
+++ test-violetear/main.go
@@ -24,20 +24,35 @@ func commonHeaders(fn http.HandlerFunc) http.HandlerFunc {
 	}
 }
 
+func wrapper(router *violetear.Router) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		router.LogRequests = true
+		fmt.Fprintf(w, "not found %s!", r.URL.Path)
+	}
+}
+
 func main() {
-	router := violetear.New(true)
+	router := violetear.New()
+	router.LogRequests = true
+	router.Request_ID = "rid"
 
 	router.AddRegex(":uuid", `[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}`)
 	router.AddRegex(":sopas", `sopas`)
 
-	router.HandleFunc("/hello", hello_world)
+	router.HandleFunc("/hello", wrapper(router), "GET")
 	router.HandleFunc("/hello/world", commonHeaders(hello_world), "GET,POST")
 	router.HandleFunc("/hello/world/:sopas", hello_world_all, "GET")
 	router.HandleFunc(":uuid", hello_world)
+	router.HandleFunc("*", hello_world)
+	router.HandleFunc("/hola/*", hello_world)
+	router.HandleFunc("/hola/:sopas", hello_world)
+	router.HandleFunc("/hola/epazote", hello_world)
+	router.HandleFunc("/hola/epazote/*", hello_world)
+	router.HandleFunc("/hola/epazote/a/papas", hello_world)
 
 	//	router.NotAllowedHandler = http.HandlerFunc(not_found)
 	//	router.NotFoundHandler = http.HandlerFunc(not_found)
+	router.SetHeader("X-app-epazote", "1.1")
 
 	router.Run(":8080")
-
 }
