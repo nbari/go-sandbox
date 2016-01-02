7a0be3917be7ca7d62a3b2733801497113148cde
nbari <nbari@dalmp.com>
2016-01-02 10:42:57 +0100

    new file:   main.go

---
+++ channels/overlap/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+type scheduler struct {
+	t    <-chan time.Time
+	quit chan struct{}
+	f    func()
+}
+
+func main() {
+
+	scheduler := scheduler{
+		t:    time.NewTicker(time.Second * 1).C,
+		quit: make(chan struct{}),
+		f: func() {
+			start := time.Now()
+			fmt.Printf("Start: %q ", start)
+			time.Sleep(time.Second * 4)
+			fmt.Printf("End: %q\n", time.Since(start))
+		},
+	}
+
+	go func() {
+		for {
+			select {
+			case <-scheduler.t:
+				scheduler.f()
+			case <-scheduler.quit:
+				return
+			}
+		}
+	}()
+
+	// exit on signal
+	block := make(chan os.Signal, 1)
+	signal.Notify(block, os.Interrupt, os.Kill, syscall.SIGTERM)
+	signalType := <-block
+	fmt.Printf("%q signal received.", signalType)
+	signal.Stop(block)
+	os.Exit(0)
+}
