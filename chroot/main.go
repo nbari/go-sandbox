2fa4193e2b86798862a9fe5ac0baec19c1273de7
nbari <nbari@dalmp.com>
2016-03-12 15:18:02 +0100

    modified:   main.go

--- chroot/main.go
+++ chroot/main.go
@@ -1,1 +1,17 @@
-http://www.sarathlakshman.com/2010/08/12/exit-from-chroot-environment-python/
+// http://www.sarathlakshman.com/2010/08/12/exit-from-chroot-environment-python/
+
+package main
+
+import (
+	"fmt"
+	"syscall"
+)
+
+func main() {
+
+	err := syscall.Chroot("/tmp/root")
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+	}
+
+}
