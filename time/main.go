acfc6cec7ffdde1754ee679f18ac73bbe7516bb5
nbari <nbari@tequila.io>
2016-06-29 09:00:27 +0200

    modified:   main.go

--- time/main.go
+++ time/main.go
@@ -6,6 +6,6 @@ import (
 )
 
 func main() {
-	t := time.Now()
-	fmt.Println("Location:", t.Location(), ":Time:", t)
+	t := time.Now().UTC()
+	fmt.Println("Location:", t.Location(), ":Time:", t.Format(time.RFC3339Nano))
 }
