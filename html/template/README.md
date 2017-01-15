6da35edf883e11ddbbbdf5e4ad2370e363f7c043
nbari <nbari@tequila.io>
2017-01-15 16:08:32 +0100

    template

---
+++ html/template/README.md
@@ -0,0 +1,7 @@
+# setup
+go get github.com/jteeuwen/go-bindata/...
+go get github.com/elazarl/go-bindata-assetfs/...
+
+# run
+go generate
+go build
---
+++ html/template/main.go
@@ -0,0 +1,61 @@
+package main
+
+//go:generate go-bindata-assetfs static/... templates/...
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/nbari/violetear"
+)
+
+// Model of stuff to render a page
+type Model struct {
+	Title string
+	Name  string
+}
+
+// Templates with functions available to them
+var templates = template.New("").Funcs(templateMap)
+
+// Parse all of the bindata templates
+func init() {
+	for _, path := range AssetNames() {
+		bytes, err := Asset(path)
+		if err != nil {
+			log.Panicf("Unable to parse: path=%s, err=%s", path, err)
+		}
+		templates.New(path).Parse(string(bytes))
+	}
+}
+
+// Render a template given a model
+func renderTemplate(w http.ResponseWriter, tmpl string, p interface{}) {
+	err := templates.ExecuteTemplate(w, tmpl, p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+// Well hello there
+func hello(w http.ResponseWriter, r *http.Request) {
+	params := r.Context().Value(violetear.ParamsKey).(violetear.Params)
+	model := Model{Name: params["*"].(string)}
+	renderTemplate(w, "templates/hello.html", &model)
+}
+
+// The server itself
+func main() {
+	// mux handler
+	router := violetear.New()
+
+	// Example route that takes one rest style option
+	router.HandleFunc("/hello/*", hello)
+	router.Handle("/static/*",
+		//	http.StripPrefix("/static", http.FileServer(http.Dir("./static"))),
+		http.StripPrefix("/static", http.FileServer(assetFS())),
+	)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
