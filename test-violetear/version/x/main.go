67e0e5bdaf500d9dcf41e14faef6ade2a39554ee
nbari <nbari@tequila.io>
2017-03-11 18:56:06 +0100

    new file:   x/main.go

---
+++ test-violetear/version/x/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/nbari/violetear"
+)
+
+func handleHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi,  %s!", r.URL.Path[1:])
+}
+func handleHelloV2(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "version 2")
+}
+
+func main() {
+	router := violetear.New()
+
+	router.HandleFunc("/hello", handleHello, "GET, HEAD")
+	router.HandleFunc("/hello#violetear.v3", handleHelloV2, "GET, HEAD, POST")
+
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
