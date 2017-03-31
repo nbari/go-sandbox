114dcfee8c3509742cd6c2342460ade3cd9146da
nbari <nbari@tequila.io>
2017-04-01 00:16:54 +0200

    new file:   main.go

---
+++ test-violetear/getParam/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/nbari/violetear"
+)
+
+func catchAll(w http.ResponseWriter, r *http.Request) {
+	// Get & print the content of named-param *
+	params := r.Context().Value(violetear.ParamsKey).(violetear.Params)
+	fmt.Fprintf(w, "CatchAll value:, %q", params["*"])
+}
+
+func handleUUID(w http.ResponseWriter, r *http.Request) {
+	// get router params
+	params := r.Context().Value(violetear.ParamsKey).(violetear.Params)
+	// using GetParam
+	uuid := violetear.GetParam("uuid", r)
+	// add a key-value pair to the context
+	ctx := context.WithValue(r.Context(), "key", "my-value")
+	// print current value for :uuid
+	fmt.Fprintf(w, "Named parameter: %q, uuid: %q,  key: %s",
+		params[":uuid"],
+		uuid,
+		ctx.Value("key"),
+	)
+}
+
+func main() {
+	router := violetear.New()
+
+	router.AddRegex(":uuid", `[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}`)
+
+	router.HandleFunc("*", catchAll)
+	router.HandleFunc("/:uuid", handleUUID, "GET,HEAD")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
