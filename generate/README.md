c953947d9e5bc51ea631a20a70755b1da6b0dd1b
nbari <nbari@tequila.io>
2017-05-29 19:05:56 +0200

    generate code example

---
+++ generate/README.md
@@ -0,0 +1,5 @@
+go generate will execute:
+
+    go run gen.go
+
+`gen.go` will create contributors.go
---
+++ generate/cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/nbari/go-sandbox/generate"
+
+func main() {
+	project.PrintContributors()
+}
