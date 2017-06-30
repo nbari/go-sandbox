50aaa693f607f71c447af1d7b8e46e2b63bcb8ac
nbari <nbari@tequila.io>
2017-06-30 20:57:36 +0200

    modified:   main.go

--- warmup/hamming_weight/main.go
+++ warmup/hamming_weight/main.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	println(ones(3 * 7))
+	println(ones(13))
 }
 
 func ones(x int) int {
 	n := 0
 	for x != 0 {
-		fmt.Printf("x & 1: %v     x >> 1: %v    x: %d\n", x&1, x>>1, x)
+		fmt.Printf("%08b\n", x)
 		n += x & 1
 		x = x >> 1
 	}
