5031a4aef332e76bd4e38cef689a074cb7a5299d
nbari <nbari@tequila.io>
2017-07-31 20:21:04 +0200

    modified:   main.go

--- crypto/bcrypt/main.go
+++ crypto/bcrypt/main.go
@@ -6,17 +6,23 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
 func main() {
-	password := []byte("MyDarkSecret")
+	password := "secret"
+	hash, _ := HashPassword(password) // ignore error for the sake of simplicity
 
-	// Hashing the password with the default cost of 10
-	hashedPassword, err := bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(hashedPassword))
+	fmt.Println("Password:", password)
+	fmt.Println("Hash:    ", hash)
 
-	// Comparing the password with the hash
-	err = bcrypt.CompareHashAndPassword(hashedPassword, password)
-	fmt.Println(err) // nil means it is a match
+	match := CheckPasswordHash(password, hash)
+	fmt.Println("Match:   ", match)
 }
