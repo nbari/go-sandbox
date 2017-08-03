dffa40fe84aeeeec24fefbed790adeae42f2a3df
nbari <nbari@tequila.io>
2017-08-03 23:02:06 +0200

    scrypt

---
+++ crypto/scrypt/basic/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"crypto/rand"
+	"fmt"
+
+	"golang.org/x/crypto/scrypt"
+)
+
+func GenerateSalt(size int) ([]byte, error) {
+	nonce := make([]byte, size)
+	_, err := rand.Read(nonce)
+	if err != nil {
+		return nil, err
+	}
+	return nonce, nil
+}
+
+func main() {
+	p := "secret"
+	//salt, _ := GenerateSalt(32)
+	salt := []byte("salt")
+	dk, err := scrypt.Key([]byte(p), salt, 16384, 8, 1, 32)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("len(dk) = %+v\n", len(dk))
+	fmt.Printf("dk = %x\n", dk)
+}
