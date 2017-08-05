00c71f26bbddfb2ef225e66f17e2a877b0d18d81
nbari <nbari@tequila.io>
2017-08-05 15:15:55 +0200

    modified:   crypto/scrypt/basic/main.go

--- crypto/scrypt/basic/main.go
+++ crypto/scrypt/basic/main.go
@@ -20,7 +20,7 @@ func main() {
 	p := "secret"
 	//salt, _ := GenerateSalt(32)
 	salt := []byte("salt")
-	dk, err := scrypt.Key([]byte(p), salt, 16384, 8, 1, 32)
+	dk, err := scrypt.Key([]byte(p), salt, 1<<14, 8, 1, 32)
 	if err != nil {
 		panic(err)
 	}
