cfa3b17b05933a1f8077749bc8c251d7a74dbd1d
nbari <nbari@tequila.io>
2017-08-08 01:14:34 +0200

    modified:   main.go

--- warmup/maximum-element/main.go
+++ warmup/maximum-element/main.go
@@ -12,9 +12,8 @@ func (s stack) Push(v int) stack {
 	return append(s, v)
 }
 
-func (s stack) Pop() (stack, int) {
-	l := len(s)
-	return s[:l-1], s[l-1]
+func (s stack) Pop() stack {
+	return s[:len(s)-1]
 }
 
 func main() {
@@ -22,17 +21,16 @@ func main() {
 	io := bufio.NewReader(os.Stdin)
 	fmt.Fscan(io, &n)
 	s := make(stack, 0)
-	for i := 0; i <= n; i++ {
+	for i := 0; i < n; i++ {
 		fmt.Fscan(io, &q)
 		switch q {
 		case 1:
 			fmt.Fscan(io, &q)
 			s = s.Push(q)
 		case 2:
-			s, _ = s.Pop()
+			s = s.Pop()
 		case 3:
-			s, q = s.Pop()
-			fmt.Println(q)
+			fmt.Println(s[len(s)-1])
 		}
 	}
 }
