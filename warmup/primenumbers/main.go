3771050a96aa52466428058dcc8eb7953bd17c0a
nbari <nbari@tequila.io>
2017-08-09 20:56:08 +0200

    modified:   main.go

--- warmup/primenumbers/main.go
+++ warmup/primenumbers/main.go
@@ -5,15 +5,15 @@ import "fmt"
 func main() {
 	sum := 0
 	for num := 1; num <= 100; num++ {
-		count := 0
+		isPrime := true
 		for i := 2; i <= num/2; i++ {
 			if num%i == 0 {
-				count++
+				isPrime = false
 				break
 			}
 		}
-		if count == 0 && num != 1 {
-			sum = sum + num
+		if isPrime && num != 1 {
+			sum += num
 		}
 	}
 	fmt.Printf("Sum of prime numbers is = %d\n", sum)
