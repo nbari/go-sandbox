4f44207b3338847657c397474a9f59e01e3e5cd3
nbari <nbari@tequila.io>
2017-08-13 19:28:30 +0200

    fix algo

--- warmup/pair-arrays/main.go
+++ warmup/pair-arrays/main.go
@@ -2,21 +2,18 @@ package main
 
 import "fmt"
 
-// unique paris (5,0), (3,2), (1,4)
+// unique pairs (5,0), (3,2), (1,4)
 func main() {
-	arr := []int{5, 3, 7, 0, 1, 4, 2}
+	nums := []int{5, 3, 7, 0, 1, 4, 2}
 	s := 5
 
-	input := map[int]int{}
+	m := map[int]int{}
 
-	for k, v := range arr {
-		input[v] = k
-	}
-
-	for _, v := range arr {
-		_, ok := input[s-v]
-		if ok {
-			fmt.Printf("pairs(%d, %d)\n", v, s-v)
+	for _, v := range nums {
+		if _, ok := m[s-v]; ok {
+			fmt.Printf("pairs(%d, %d)\n", s-v, v)
+		} else {
+			m[v] = s - v
 		}
 	}
 }
