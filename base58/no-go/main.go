dcd350df999e9b31ab3436d8fedf7c80f2b5c531
nbari <nbari@tequila.io>
2017-08-19 18:02:20 +0200

    modified:   main.go 	new file:   no-go/main.go

--- base58/no-go/main.go
+++ base58/no-go/main.go
@@ -2,24 +2,17 @@ package main
 
 import (
 	"fmt"
-	"sync"
 
 	"github.com/nbari/base58"
 )
 
 func encode(i uint64) {
 	x := base58.Encode(i)
-	fmt.Printf("%d = %s\n", i, x)
+	fmt.Printf("%d = %+v\n", i, x)
 }
 
 func main() {
-	var wg sync.WaitGroup
 	for i, val := uint64(0), uint64(1<<24); i <= val; i++ {
-		wg.Add(1)
-		go func(j uint64, wg *sync.WaitGroup) {
-			encode(j)
-			wg.Done()
-		}(i, &wg)
+		encode(i)
 	}
-	wg.Wait()
 }
