361b5d165905cc44b67e63b69e693a9ba1839d08
nbari <nbari@tequila.io>
2017-08-28 18:42:58 +0200

    modified:   main.go

--- warmup/pair-arrays/main.go
+++ warmup/pair-arrays/main.go
@@ -7,13 +7,13 @@ func main() {
 	nums := []int{5, 3, 7, 0, 1, 4, 2}
 	s := 5
 
-	m := map[int]int{}
+	m := map[int]struct{}{}
 
 	for _, v := range nums {
 		if _, ok := m[s-v]; ok {
 			fmt.Printf("pairs(%d, %d)\n", s-v, v)
 		} else {
-			m[v] = s - v
+			m[v] = struct{}{}
 		}
 	}
 }
