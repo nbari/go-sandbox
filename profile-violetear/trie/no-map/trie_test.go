8615e59ca8086d7279c24eb2b5db351fa7fd6e1f
nbari <nbari@tequila.io>
2017-09-24 23:03:58 +0200

    modified:   Makefile

--- profile-violetear/trie/no-map/trie_test.go
+++ profile-violetear/trie/no-map/trie_test.go
@@ -5,16 +5,26 @@ import (
 	"testing"
 )
 
-func BenchmarkTrie(b *testing.B) {
-	b.ReportAllocs()
-	b.ResetTimer()
-
+func TestTrie(t *testing.T) {
 	trie := &Trie{}
-	path := []string{"/"}
 
-	for i := 0; i < b.N; i++ {
-		trie.Set(path, nil, "ALL", "v3")
-		trie.Set(path, nil, "ALL", "")
-		path = append(path, fmt.Sprintf("%d", i))
+	path := []string{"/"}
+	trie.Set(path, nil, "ALL", "")
+
+	path = []string{"/zero"}
+	trie.Set(path, nil, "ALL", "")
+
+	path = []string{"/", "foo"}
+	trie.Set(path, nil, "ALL", "")
+
+	path = []string{"/", "bar", ""}
+	trie.Set(path, nil, "ALL", "")
+
+	fmt.Printf("trie = %+v\n", trie)
+	for _, n := range trie.Node {
+		fmt.Printf("n = %+v\n", n)
+		for _, nn := range n.Node {
+			fmt.Printf("nn = %+v\n", nn)
+		}
 	}
 }
