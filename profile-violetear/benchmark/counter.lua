2baa0d222a1200eee85c2a0ecb20695c830040ee
nbari <nbari@tequila.io>
2017-09-29 17:12:31 +0200

    update

--- profile-violetear/benchmark/counter.lua
+++ profile-violetear/benchmark/counter.lua
@@ -13,9 +13,13 @@ end
 done = function(summary, latency, requests)
   -- open output file
   f = io.open("results/wrk.csv", "a+")
-  f:write("time_started","min_latency,max_latency,mean_latency,stdev,50th,90th,99th,99.999th,duration,requests,bytes,connect_errors,read_errors,write_errors,status_errors,timeouts\n")
-  f:write(string.format("%s,%f,%f,%f,%f,%f,%f,%f,%f,%d,%d,%d,%d,%d,%d,%d\n",
+  f:write("time_started,min_requests,max_requests,mean_requests,count_requests,min_latency,max_latency,mean_latency,stdev,50th,90th,99th,99.999th,duration,requests,bytes,connect_errors,read_errors,write_errors,status_errors,timeouts\n")
+  f:write(string.format("%s,%f,%f,%f,%s,%f,%f,%f,%f,%f,%f,%f,%f,%d,%d,%d,%d,%d,%d,%d\n",
     os.date("!%Y-%m-%dT%TZ"),
+    requests.min,   -- minimum latency
+    requests.max,   -- max latency
+    requests.mean,  -- mean of latency
+    requests, -- raw value and count
     latency.min,    -- minimum latency
     latency.max,    -- max latency
     latency.mean,   -- mean of latency
