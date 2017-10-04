0c6c7cf4f119f808ce9ffa0f8f34d678858cb44f
nbari <nbari@tequila.io>
2017-10-04 17:14:29 +0200

    update

---
+++ profile-violetear/splitpath/no-split/main/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func noSplit(path string) {
+	var field bytes.Buffer
+	for _, rune := range path {
+		if rune == '/' {
+			if field.Len() > 0 {
+				fmt.Printf("field = %+v\n", field.String())
+				field.Reset()
+			}
+			continue
+		}
+		field.WriteRune(rune)
+	}
+	if field.Len() > 0 {
+		fmt.Printf("field = %+v\n", field.String())
+	}
+}
+
+func main() {
+	paths := []string{
+		"/hello",
+		"/hello/world",
+		"/hello/:uuid",
+		"/hello/:uuid/:uuid",
+		"/hello/:uuid/:uuid/*",
+		"/hello/:uuid/:uuid/*/",
+	}
+	for _, p := range paths {
+		fmt.Printf("p = %+v\n", p)
+		noSplit(p)
+	}
+}
