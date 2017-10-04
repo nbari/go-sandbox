873c63f4d90ff3807e7f84038a925f3b35c2cdc2
nbari <nbari@tequila.io>
2017-10-04 18:08:56 +0200

    update

--- profile-violetear/splitpath/no-split/main/main.go
+++ profile-violetear/splitpath/no-split/main/main.go
@@ -7,37 +7,33 @@ import (
 
 func noSplit(path string) {
 	var field bytes.Buffer
-	for _, rune := range path {
-		if rune == '/' {
-			if field.Len() > 0 {
-				fmt.Printf("field = %+v\n", field.String())
-				field.Reset()
-			}
-			continue
+	for i, rune := range path {
+		if rune == '/' && i > 0 {
+			fmt.Printf("field = %+v\n", field.String())
+			fmt.Printf("path = %+v\n", path[i:])
+			return
 		} else if rune == '*' {
 			fmt.Printf("field = %c\n", rune)
-			field.Reset()
 			return
+		} else if rune != '/' {
+			field.WriteRune(rune)
 		}
-		field.WriteRune(rune)
 	}
 	if field.Len() > 0 {
 		fmt.Printf("field = %+v\n", field.String())
+		fmt.Printf("path = %+v\n", "")
 	}
 }
 
 func main() {
 	paths := []string{
-		"/hello",
-		"/hello/world",
-		"/hello/:uuid",
 		"/hello/:uuid/:uuid",
-		"/hello/:uuid/:uuid/*",
-		"/hello/:uuid/:uuid/*/",
-		"/hello/*/:uuid/:uuid/*/",
+		"/:uuid/:uuid",
+		"/:uuid",
 	}
 	for _, p := range paths {
 		fmt.Printf("p = %+v\n", p)
 		noSplit(p)
+		println()
 	}
 }
