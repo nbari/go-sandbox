aae606fc24954c498db60261ded018fe7e3a4a3a
nbari <nbari@tequila.io>
2017-10-07 11:09:45 +0200

    update

---
+++ test-violetear/logger/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/nbari/violetear"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello world!"))
+}
+
+func myLogger(w *violetear.ResponseWriter, r *http.Request) {
+	j := map[string]interface{}{
+		"Time":        time.Now().UTC().Format(time.RFC3339),
+		"RemoteAddr":  r.RemoteAddr,
+		"URL":         r.URL.String(),
+		"Status":      w.Status(),
+		"Size":        w.Size(),
+		"RequestTime": w.RequestTime(),
+		"RequestID":   w.RequestID(),
+	}
+	if err := json.NewEncoder(os.Stdout).Encode(j); err != nil {
+		log.Println(err)
+	}
+}
+
+func main() {
+	router := violetear.New()
+	router.LogRequests = true
+	router.Logger = myLogger
+	router.Verbose = false
+	router.HandleFunc("*", helloWorld)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
