3000cb48925fc92e9e81de1b25af3abfaef34b02
nbari <nbari@tequila.io>
2017-10-08 17:18:18 +0200

    udpate

--- profile-violetear/benchmark/start.sh
+++ profile-violetear/benchmark/start.sh
@@ -9,90 +9,35 @@ write() {
     echo \#----------------------------------------------------------------------------'\033[0m'
 }
 
-write "100 sleeping 0ms"
-./benchmark > results/go-stats.csv &
+for t in 0 10 100 500
+do
+write "100 sleeping ${t}ms"
+./benchmark -s ${t} >> results/go-stats.csv &
 sleep 1
 wrk -c100 -d15s -t2 -s counter.lua http://0:8080
 pkill -9 benchmark
 sleep 2
+done
 
-write "100 sleeping 10ms"
-./benchmark -s 10 >> results/go-stats.csv &
-sleep 1
-wrk -c100 -d15s -t2 -s counter.lua http://0:8080
-pkill -9 benchmark
-sleep 2
-
-write "100 sleeping 100ms"
-./benchmark -s 100 >> results/go-stats.csv &
-sleep 1
-wrk -c100 -d15s -t2 -s counter.lua http://0:8080
-pkill -9 benchmark
-sleep 2
-
-write "100 sleeping 500ms"
-./benchmark -s 500 >> results/go-stats.csv &
-sleep 1
-wrk -c100 -d15s -t2 -s counter.lua http://0:8080
-pkill -9 benchmark
-sleep
-
-write "300 sleeping 0ms"
-./benchmark >> results/go-stats.csv &
+for t in 0 10 100 500
+do
+write "300 sleeping ${t}ms"
+./benchmark -s ${t} >> results/go-stats.csv &
 sleep 1
 wrk -c300 -d15s -t2 -s counter.lua http://0:8080
 pkill -9 benchmark
 sleep 2
+done
 
-write "300 sleeping 10ms"
-./benchmark -s 10 >> results/go-stats.csv &
-sleep 1
-wrk -c300 -d15s -t2 -s counter.lua http://0:8080
-pkill -9 benchmark
-sleep 2
-
-write "300 sleeping 100ms"
-./benchmark -s 100 >> results/go-stats.csv &
-sleep 1
-wrk -c300 -d15s -t2 -s counter.lua http://0:8080
-pkill -9 benchmark
-sleep 2
-
-write "300 sleeping 500ms"
-./benchmark -s 500 >> results/go-stats.csv &
-sleep 1
-wrk -c300 -d15s -t2 -s counter.lua http://0:8080
-pkill -9 benchmark
-sleep 2
-
-write "500 sleeping 0ms"
-./benchmark >> results/go-stats.csv &
+for t in 0 10 100 500
+do
+write "500 sleeping ${t}ms"
+./benchmark -s ${t} >> results/go-stats.csv &
 sleep 1
 wrk -c500 -d15s -t2 -s counter.lua http://0:8080
 pkill -9 benchmark
 sleep 2
-
-write "500 sleeping 10ms"
-./benchmark -s 10 >> results/go-stats.csv &
-sleep 1
-wrk -c500 -d15s -t2 -s counter.lua http://0:8080
-pkill -9 benchmark
-sleep 2
-
-write "500 sleeping 100ms"
-./benchmark -s 100 >> results/go-stats.csv &
-sleep 1
-wrk -c500 -d15s -t2 -s counter.lua http://0:8080
-pkill -9 benchmark
-sleep 2
-
-write "500 sleeping 500ms"
-./benchmark -s 500 >> results/go-stats.csv &
-sleep 1
-wrk -c500 -d15s -t2 -s counter.lua http://0:8080
-pkill -9 benchmark
-sleep 2
-
+done
 
 END=$(date +%s)
 DIFF=$(echo "$END - $START" | bc)
