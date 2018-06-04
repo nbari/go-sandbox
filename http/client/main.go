bb4215dffed3c61fa97f07319dfb685f43521f01
nbari <nbari@tequila.io>
2018-06-04 11:32:11 +0200

    sync Mon Jun  4 11:32:11 CEST 2018

--- http/client/main.go
+++ http/client/main.go
@@ -2,18 +2,33 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func main() {
-	response, err := http.Get("https://google.com")
-	if err != nil {
-		log.Fatal(err)
+	var (
+		err      error
+		response *http.Response
+		retries  int = 3
+	)
+	for retries > 0 {
+		// response, err = http.Get("https://non-existen")
+		response, err = http.Get("https://google.com/robots.txt")
+		if err != nil {
+			log.Println(err)
+			retries -= 1
+		} else {
+			break
+		}
 	}
-	defer response.Body.Close()
-	data, err := ioutil.ReadAll(io.LimitReader(response.Body, 40000))
-	fmt.Printf("data = %sn", data)
+	if response != nil {
+		defer response.Body.Close()
+		data, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("data = %s\n", data)
+	}
 }
