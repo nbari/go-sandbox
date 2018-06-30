d888a630dcbfa60154e4c50524a77cf679b489b0
nbari <nbari@tequila.io>
2018-06-30 14:34:31 +0200

    sync Sat Jun 30 14:34:31 CEST 2018

--- emoji/main.go
+++ emoji/main.go
@@ -1,9 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
 
-const logo = '\u2B55'
+func Flag(x string) (string, error) {
+	x = strings.ToUpper(x)
+	if len(x) != 2 {
+		return "", errors.New("country code must be two letters")
+	}
+	if x[0] < 'A' || x[0] > 'Z' || x[1] < 'A' || x[1] > 'Z' {
+		return "", errors.New("invalid country code")
+	}
+	return string(0x1F1E6+rune(x[0])-'A') + string(0x1F1E6+rune(x[1])-'A'), nil
+}
 
 func main() {
-	fmt.Printf("logo = %c\n", logo)
+	flag.Parse()
+	if flag.NArg() == 0 {
+		fmt.Fprintln(os.Stderr, "Enter a country code")
+		os.Exit(1)
+	}
+	f, err := Flag(flag.Arg(0))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Flag: %s\n", f)
 }
