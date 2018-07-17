52163d016e2ba0dfa685ef9e6dfb35e4829bf9c2
nbari <nbari@tequila.io>
2018-07-17 14:36:15 +0200

    sync Tue Jul 17 14:36:15 CEST 2018

--- time/duration/main.go
+++ time/duration/main.go
@@ -7,10 +7,14 @@ import (
 )
 
 func main() {
-	d, err := time.ParseDuration("1m30s")
+	// d, err := time.ParseDuration("1m30s")
+	d, err := time.ParseDuration("10s")
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf("m.Seconds() = %+v\n", d.Seconds())
 	fmt.Printf("d = %+T\n", d)
+	fmt.Println("Unix:  ", time.Now().Unix())
+	t := time.Now().Add(d)
+	fmt.Println("Unix:+ ", t.Unix())
 }
