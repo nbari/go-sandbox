dabcb620705551961c38aa9d975464fe947844c4
nbari <nbari@tequila.io>
2019-04-29 14:58:36 +0200

    sync Mon Apr 29 14:58:36 CEST 2019

--- test-violetear/499/example/main.go
+++ test-violetear/499/example/main.go
@@ -7,7 +7,31 @@ import (
 	"time"
 
 	"github.com/nbari/violetear"
+	"github.com/nbari/violetear/middleware"
 )
+
+func catch499(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		log.Println("Starting middleware")
+		defer log.Println("End of middleware")
+
+		ch := make(chan struct{})
+
+		go func(ch chan struct{}) {
+			time.Sleep(3 * time.Second)
+			ch <- struct{}{}
+		}(ch)
+
+		select {
+		case <-ch:
+			next.ServeHTTP(w, r)
+		case <-ctx.Done():
+			http.Error(w, ctx.Err().Error(), 499)
+			return
+		}
+	})
+}
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
@@ -31,7 +55,9 @@ func main() {
 	router := violetear.New()
 	router.LogRequests = true
 
-	router.HandleFunc("/", helloWorld, "GET")
+	chain := middleware.New(catch499)
+
+	router.Handle("/", chain.ThenFunc(helloWorld), "GET")
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
