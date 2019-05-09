b2fddae31ecfecd5241b0e2f230573e3422824f7
nbari <nbari@tequila.io>
2019-05-09 11:06:28 +0200

    sync Thu May  9 11:06:28 CEST 2019

--- warmup/pair-arrays/complement.go
+++ warmup/pair-arrays/complement.go
@@ -6,8 +6,9 @@ import "fmt"
 
 // unique pairs (5,0), (3,2), (1,4)
 func main() {
-	nums := []int{5, 3, 7, 0, 1, 4, 2}
-	s := 5
+	//nums := []int{5, 3, 7, 0, 1, 4, 2}
+	nums := []int{1, 5, 7, -1, 5}
+	s := 6
 
 	m := map[int]struct{}{}
 
