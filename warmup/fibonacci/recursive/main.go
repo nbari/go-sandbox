0f1924ffebb93940af1f4fbb58bdb643889a76c0
nbari <nbari@tequila.io>
2019-05-15 21:53:05 +0200

    sync Wed May 15 21:53:05 CEST 2019

--- warmup/fibonacci/recursive/main.go
+++ warmup/fibonacci/recursive/main.go
@@ -4,22 +4,14 @@ import (
 	"fmt"
 )
 
-var seq = []int{0, 1}
-
 func fib(n int) int {
 	if n < 2 {
 		return n
 	}
-	seq = append(seq, fib(seq[n-1])+fib(seq[n-2]))
-	fmt.Printf("seq = %+v\n", seq)
-	fmt.Printf("n = %+v\n", n)
-	if n >= 10 {
-		return n
-	}
-	return fib(n + 1)
+	return fib(n-1) + fib(n-2)
 }
 
 func main() {
 	// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
-	fmt.Println(fib(2))
+	fmt.Println(fib(7))
 }
