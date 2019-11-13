36198ee9b3352edf8e9c92f2551b018bdf2fb20b
nbari <nbari@tequila.io>
2019-11-13 19:19:31 +0100

    sync Wed Nov 13 19:19:31 CET 2019

---
+++ test-violetear/hello_world/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/nbari/violetear"
+)
+
+func hello_world(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello world!")
+}
+
+func main() {
+	router := violetear.New()
+	router.LogRequests = false
+
+	router.HandleFunc("/hello/world", hello_world)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
